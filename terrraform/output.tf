228417c4acd7da6a69c1a1de5bd9f23e89b3cc65
esraaghazal <120502146+esraaghazal@users.noreply.github.com>
2026-02-25 17:41:09 +0200

    Rename output variables and update values

--- terrraform/output.tf
+++ terrraform/output.tf
@@ -1,9 +1,11 @@
-output "frontend_ip"  { 
-    value = aws_instance.frontend.public_ip 
-    }
-output "backend_ip"   { 
-    value = aws_instance.backend.public_ip 
-    }
-output "rds_endpoint" { 
-    value = aws_db_instance.mysql.endpoint 
-    }
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_private_ip" {
+  value = aws_instance.backend.private_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds.endpoint
+}
