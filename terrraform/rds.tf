e2337abd593748a618a0230c131077ca21d02af0
esraaghazal <120502146+esraaghazal@users.noreply.github.com>
2026-02-25 17:40:42 +0200

    Refactor RDS instance and add subnet group
    Refactor RDS configuration by creating a subnet group and updating the DB instance.

--- terrraform/rds.tf
+++ terrraform/rds.tf
@@ -1,20 +1,19 @@
-resource "aws_db_instance" "mysql" {
-  identifier              = "app-db"
-  engine                  = "mysql"
-  engine_version          = "8.0"
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-  storage_type            = "gp3"
-  db_name                 = "appdb"
-  username                = "admin"
-  # pass to acces the db
-  password                = var.db_password
-  # can not access the internet 
-  publicly_accessible     = false
-  vpc_security_group_ids  = [aws_security_group.rds.id]
-  db_subnet_group_name    = aws_db_subnet_group.private.name
-  # do notmake a snap when we delete the rds
-  skip_final_snapshot     = true
+resource "aws_db_subnet_group" "private" {
+  name = "private-db-subnet"
 
-  tags = { Name = "app-mysql" }
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+}
+
+resource "aws_db_instance" "rds" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.private.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  skip_final_snapshot    = true
 }
