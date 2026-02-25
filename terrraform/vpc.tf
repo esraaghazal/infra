a148f67c23479c1b57b09964d89c341c6c769a69
esraaghazal <120502146+esraaghazal@users.noreply.github.com>
2026-02-25 17:33:42 +0200

    Configure VPC with public and private subnets
    Added public and private subnets with NAT gateway configuration.

--- terrraform/vpc.tf
+++ terrraform/vpc.tf
@@ -1,3 +1,5 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -5,60 +7,86 @@ resource "aws_vpc" "main" {
   tags = { Name = "task-vpc" }
 }
 
-######### gateway ############
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags   = { Name = "task-igw" }
 }
 
-####### public subnet ###########
-
+# Public Subnets
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-central-1a"
-  #  when ec2 launch it will take public ip 
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
-  tags                    = { Name = "public-a" }
 }
 
-########## private subnet ###########
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+}
 
+# Private Subnets
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-central-1a"
-  tags              = { Name = "private-a" }
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
 }
 
-############# db subnet ##############
-# when we create rds use subnet from this subnet ids dont create it in another subnet 
-resource "aws_db_subnet_group" "private" {
-  name       = "private-db-subnet"
-  subnet_ids = [aws_subnet.private_a.id]
-  tags       = { Name = "private-db-subnet-group" }
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
 
-## route teble
+# NAT
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+}
+
+# Route Tables
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  tags   = { Name = "public-rt" }
 }
 
-# any trrafic come to 0.0.0.0/0 forwared it to the gaeway
-
-resource "aws_route" "internet" {
+resource "aws_route" "public_internet" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-# connect the public subnet with route table 
-
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
